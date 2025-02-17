6bb9ff4474dddbf8460bc5275d6f394a1f73f9c3
jaimelgc <jaimeleongarcia123@gmail.com>
2025-02-17 16:05:21 +0000

    basic book list view #8

--- backend/userprofile/views.py
+++ backend/userprofile/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .models import BookList
+
+
+@login_required
+def create_book_list(request):
+    if request.method == "POST":
+        name = request.POST.get("name").strip()
+        description = request.POST.get("description", "").strip()
+
+        if name:
+            BookList.objects.create(user=request.user, name=name, description=description)
+            return redirect("user_book_lists")
+
+    return render(request, "create_book_list.html")
