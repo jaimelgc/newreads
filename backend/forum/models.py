43112293f2a3a63b1998da6cdc84a946fb19295c
jaimelgc <jaimeleongarcia123@gmail.com>
2025-03-08 14:36:05 +0000

    forum models

--- backend/forum/models.py
+++ backend/forum/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from ..library.models import Work
+from ..userprofile.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    poster = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="user_posts"
+    )
+    book = models.ForeignKey(
+        Work, on_delete=models.SET_NULL, null=True, blank=True, related_name="book_posts"
+    )
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    poster = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="posts")
+    original_post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    quoted_post = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="quoted"
+    )
+    quote = models.TextField(blank=True, null=True)
+
+    # [quote=Kage Goomba;591764935569058073]This thread is full of win. [/quote]
