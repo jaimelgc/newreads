b5029dff4f6ee8b485b50d66e498e7d37c042170
jaimelgc <jaimeleongarcia123@gmail.com>
2025-03-26 15:14:26 +0000

    user related serializers and password hashing

--- backend/newreads/urls.py
+++ backend/newreads/urls.py
@@ -23,4 +23,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('library/', include('library.urls')),
+    path('user/', include('userprofile.urls')),
+    path('forum/', include('forum.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
