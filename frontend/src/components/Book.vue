f96f97a2ce9ccc97c16822860c3a49fa608b0d52
jaimelgc <jaimelg016@gmail.com>
2025-04-22 16:46:25 +0100

    save

--- frontend/src/components/Book.vue
+++ frontend/src/components/Book.vue
@@ -1,1 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import { defineProps, ref, computed } from 'vue';
+
+    const props = defineProps({
+        book: Object
+    })
+
+    const showFullDescription = ref(false)
+
+    const toggleFullDescription = () => {
+        showFullDescription.value = !showFullDescription.value
+    }
+
+    const truncateDescription = computed(() => {
+        let description = props.book.description; // check description? ERROR ts
+        if (!showFullDescription.value) {
+            description = description.substring(0,90) + '...';
+        }
+        return description;
+    });
+
+</script>
