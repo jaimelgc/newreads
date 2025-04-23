65022e06d6b4a95bf8255d29170ffc45a13781b1
jaimelgc <jaimelg016@gmail.com>
2025-04-23 16:05:01 +0100

    save

--- frontend/src/components/Book.vue
+++ frontend/src/components/Book.vue
@@ -24,7 +24,7 @@
     }
 
     const truncateDescription = computed(() => {
-        let description = props.book.description; // check description? ERROR ts
+        let description = props.book.description;
         if (!showFullDescription.value) {
             description = description.substring(0,90) + '...';
         }
@@ -37,30 +37,17 @@
     <div class="bg-white rounded-xl shadow-md relative">
         <div class="p-4">
             <div class="mb-6">
-                <div class="text-gray-600 my-2">{{ job.type }}</div>
-                <h3 class="text-xl font-bold">{{ job.title }}</h3>
+                <div class="text-gray-600 my-2">{{ book.title }}</div>
             </div>
-
             <div class="mb-5">
                 {{ truncateDescription }}
                 <button @click="toggleFullDescription" class="text-green-500 hover:text-green-600">
                     {{ showFullDescription ? 'See Less' : 'See More' }}
                 </button>
             </div>
-
-            <h3 class="text-green-500 mb-2">{{ job.salary }} / Year</h3>
-
-            <div class="border border-gray-100 mb-5"></div>
-
-            <div class="flex flex-col lg:flex-row justify-between mb-4">
-                <div class="text-orange-700 mb-3">
-                    <i class="pi pi-map-marker text-orange-700"></i>
-                    {{ job.location }}
-                </div>
-                <RouterLink :to="'/jobs/' + job.id" class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm">
-                    Read More
-                </RouterLink>
-            </div>
+            <!-- <RouterLink :to="'/jobs/' + job.id" class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm">
+                Read More
+            </RouterLink> -->
         </div>
     </div>
 </template>
