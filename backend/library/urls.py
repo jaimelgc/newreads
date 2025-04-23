439e9a29ed42da3f67e20d56b79017bf37543820
jaimelgc <jaimelg016@gmail.com>
2025-04-23 18:24:04 +0100

    save

--- backend/library/urls.py
+++ backend/library/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import OpenLibrarySearchView
 
 urlpatterns = [
-    path('search', OpenLibrarySearchView.as_view(), name='search_and_catch'),
+    path('search/', OpenLibrarySearchView.as_view(), name='search_and_catch'),
 ]
