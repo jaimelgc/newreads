57ca0b3c9f823a3a1849e16a1a5671dac908069d
jaimelgc <jaimelg016@gmail.com>
2025-04-28 19:13:02 +0100

    view structure

--- frontend/src/components/Books.vue
+++ frontend/src/components/Books.vue
@@ -1,5 +1,30 @@
 <script setup lang="ts">
     import Book from './Book.vue';
+    import { onMounted, reactive } from 'vue';
+    import api from '@/api';
+   
+    interface showButton {
+        type: boolean
+        default: false
+    }
+
+    const state = reactive({
+        books: [],
+        isLoading: true
+    });
+
+    const props = defineProps<{ showButton: showButton, limit: Number }>()
+
+    onMounted(async () => {
+        try {
+            const response = await api.get('ruta');
+            state.books = response.data;
+        } catch (error) {
+            console.error('Error fetching jobs');
+        } finally {
+            state.isLoading = false;
+        }
+    });
 </script>
 
 <template>
