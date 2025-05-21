cf0a688963d478789d2c338263d8fac8e53c7100
jaimelgc <jaimeleongarcia123@gmail.com>
2025-05-21 12:48:48 +0100

    database

--- backend/newreads/settings.py
+++ backend/newreads/settings.py
@@ -93,20 +93,20 @@ WSGI_APPLICATION = 'newreads.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(default=config('DATABASE_URL'))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
 
-# # Django DB backend is used with mysql-connector
-# if DATABASES['default']['ENGINE'] == 'django.db.backends.mysql':
-#     DATABASES['default']['ENGINE'] = 'mysql.connector.django'
+DATABASES = {
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
+}
+
+# Django DB backend is used with mysql-connector
+if DATABASES['default']['ENGINE'] == 'django.db.backends.mysql':
+    DATABASES['default']['ENGINE'] = 'mysql.connector.django'
 
 
 # Password validation
